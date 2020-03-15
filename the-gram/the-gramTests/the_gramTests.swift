ca301459b7d7ee5eef79a3246c2d40c5cc7cd84a
Tommy Goossens <tommygoossens@ziggo.nl>
2020-03-15 13:14:36 +0100

    Default test targets

---
+++ the-gram/the-gramTests/the_gramTests.swift
@@ -0,0 +1,34 @@
+//
+//  the_gramTests.swift
+//  the-gramTests
+//
+//  Created by Tommy Goossens on 10/03/2020.
+//  Copyright © 2020 Tommy Goossens. All rights reserved.
+//
+
+import XCTest
+@testable import The_Gram
+
+class the_gramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
